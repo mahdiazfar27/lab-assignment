ae4090af278f648048911c711a8a313886d56339
mahdiazfar27 <mahdi.azfar.talukder@gmail.com>
2025-09-07 11:12:16 +0600

    Added static calendar and time slots to Services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -23,6 +23,46 @@ const Services = () => {
             </select>
           </div>
         </div>
+
+        {/* New Calendar Section */}
+        <div className="calendar-container">
+          <div className="calendar-header">
+            <button className="calendar-nav">&lt;</button>
+            <h3>July 2024</h3>
+            <button className="calendar-nav">&gt;</button>
+          </div>
+          <div className="calendar-grid">
+            {['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'].map(day => (
+              <div key={day} className="calendar-day-name">{day}</div>
+            ))}
+            {/* Generating static dates for display */}
+            {[...Array(31).keys()].map(i => {
+              const day = i + 1;
+              let classNames = "calendar-day";
+              if (day === 5) classNames += " selected";
+              if (day < 1 || day > 31) classNames += " disabled";
+              return <div key={i} className={classNames}>{day}</div>
+            })}
+          </div>
+        </div>
+
+        {/* New Available Times Section */}
+        <div className="available-times-container">
+            <h4>Available Times on July 5, 2024</h4>
+            <div className="time-slots-grid">
+                {['9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM'].map((time, index) => (
+                    <button key={index} className={`time-slot ${time === '10:30 AM' ? 'selected' : ''} ${time === '12:00 PM' || time === '5:30 PM' ? 'disabled' : ''}`}>
+                        {time}
+                    </button>
+                ))}
+            </div>
+        </div>
+        
+        {/* New Confirm Button */}
+        <div className="confirm-booking-container">
+          <button className="confirm-booking-btn">Confirm Booking</button>
+        </div>
+
       </div>
     </div>
   );
