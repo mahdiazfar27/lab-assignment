984f7c920b27e1bdce7df6a229607e76423bb54e
mahdiazfar27 <mahdi.azfar.talukder@gmail.com>
2025-09-07 11:50:09 +0600

    Finallay added the profile photoes of the doctors

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -1,76 +1,92 @@
 // src/dummy-data.js
 
+
+import doc1 from './components/assets/images/1.webp';
+import doc2 from './components/assets/images/2.jpg';
+import doc3 from './components/assets/images/3.jpg';
+import doc4 from './components/assets/images/4.webp';
+import doc5 from './components/assets/images/5.jpg';
+import doc6 from './components/assets/images/6.jpg';
+import doc7 from './components/assets/images/7.jpg';
+import doc8 from './components/assets/images/8.webp';
+import doc9 from './components/assets/images/9.jpg';
+import doc10 from './components/assets/images/10.jpg';
+import doc11 from './components/assets/images/11.jpg';
+import doc12 from './components/assets/images/12.jpg';
+
+
+
 export const doctors = [
   {
     id: 1,
     name: "Dr. Anika Rahman",
     email: "anika.rahman@smile.studio",
-    imageUrl: "https://i.imgur.com/8LTtJg1.jpeg",
+    imageUrl: doc1,
   },
   {
     id: 2,
     name: "Dr. Ben Carter",
     email: "ben.carter@smile.studio",
-    imageUrl: "https://i.imgur.com/vH12W2v.jpeg",
+    imageUrl: doc2,
   },
   {
     id: 3,
     name: "Dr. Chloe Davis",
     email: "chloe.davis@smile.studio",
-    imageUrl: "https://i.imgur.com/D47v980.jpeg",
+    imageUrl: doc3,
   },
   {
     id: 4,
     name: "Dr. David Evans",
     email: "david.evans@smile.studio",
-    imageUrl: "https://i.imgur.com/eYnrcB4.jpeg",
+    imageUrl: doc4,
   },
   {
     id: 5,
     name: "Dr. Emily Frank",
     email: "emily.frank@smile.studio",
-    imageUrl: "https://i.imgur.com/tIfr4s2.jpeg",
+    imageUrl: doc5,
   },
   {
     id: 6,
     name: "Dr. Fiona Green",
     email: "fiona.green@smile.studio",
-    imageUrl: "https://i.imgur.com/kSwa2hN.jpeg",
+    imageUrl: doc6,
   },
   {
     id: 7,
     name: "Dr. George Hill",
     email: "george.hill@smile.studio",
-    imageUrl: "https://i.imgur.com/pZqFYy5.jpeg",
+    imageUrl: doc7,
   },
   {
     id: 8,
     name: "Dr. Hannah Ivy",
     email: "hannah.ivy@smile.studio",
-    imageUrl: "https://i.imgur.com/iP5gG8c.jpeg",
+    imageUrl: doc8,
   },
   {
     id: 9,
     name: "Dr. Ian Johnson",
     email: "ian.johnson@smile.studio",
-    imageUrl: "https://i.imgur.com/ZzRZx7R.jpeg",
+    imageUrl: doc9,
   },
   {
     id: 10,
     name: "Dr. Jack King",
     email: "jack.king@smile.studio",
-    imageUrl: "https://i.imgur.com/uABi3t1.jpeg",
+    imageUrl: doc10,
   },
   {
     id: 11,
     name: "Dr. Liam Smith",
     email: "liam.smith@smile.studio",
-    imageUrl: "https://i.imgur.com/Cq222N3.jpeg"
+    imageUrl: doc11
   },
   {
     id: 12,
     name: "Dr. Nora Jones",
     email: "nora.jones@smile.studio",
-    imageUrl: "https://i.imgur.com/P5n8bN8.jpeg"
+    imageUrl: doc12
   },
 ];
